d246f52aa824811b5c2ef8b49883bf835439c029
Orlando Flores Teomitzi <orlando.floresti@gmail.com>
2023-06-25 00:03:25 -0600

    More examples for dart types

--- 02-variables/bin/main.dart
+++ 02-variables/bin/main.dart
@@ -1,15 +1,59 @@
 /**
  * Main entry point
  */
+import 'dart:io';
+import 'dart:async';
 
 main() {
   // boolType();
   // numberType();
-  stringType();
+  // stringType();
+  // consts();
+  userInput();
+}
+
+
+userInput() {
+  stdout.write('What is your name?\r\n');
+  String? name = stdin.readLineSync();
+
+  name == null ? stderr.write('Name is empty') :  stdout.write('Hello ${name}');
+}
+
+consts() {
+  String hello = 'Hello';
+
+  // if const is declared this variable will never change
+  const String world = 'world';
+
+  print(hello + ' ' + world);
 }
 
 stringType() {
+  String hello = 'Hello world';
+  String name = 'Orlando Flores';
+  print('Hello ${name}');
+  
+  // get substring
+  String firstname = name.substring(0,7);
+  print(firstname);
+  
+  // get the index of a string
+  int index = name.indexOf(' ');
+  String lastname = name.substring(index).trim();
+  print('lastname = ${lastname}');
 
+  // length
+  print(name.length);
+
+  // Contains
+  print(name.contains('or'));
+
+  // Create a list
+  List parts = name.split(' ');
+  print(parts);
+  print(parts[0]);
+  print(parts[1]);
 }
 
 numberType() {
