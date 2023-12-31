d2bcb9c70a4c60b8edce16047ba80ea3df6852ac
Orlando Flores Teomitzi <orlando.floresti@gmail.com>
2023-12-31 05:52:09 -0600

    error handling

---
+++ 06-error-handling/main.dart
@@ -0,0 +1,23 @@
+void main(List<String> args) {
+  try {
+    var age = null;
+    int dogYears = 7;
+
+    if(age == null) throw new UnimplementedError();
+
+    print(age * dogYears);
+  } 
+  on UnimplementedError {
+    print('The value was null!!!');
+  }
+  on NoSuchMethodError {
+    print('Sorry thats not going to happen');
+  }
+  catch ( e ) {
+    print('There was an error: ${e.toString()}');
+  } 
+  finally {
+    print('Complete');
+  }
+ 
+}
