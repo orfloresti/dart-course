e8536be2aff892bb4a12eb4b13e910a310b4c279
Orlando Flores Teomitzi <orlando.floresti@gmail.com>
2024-01-11 18:42:07 -0600

    Public and private scope

---
+++ 12-scope/animal.dart
@@ -0,0 +1,8 @@
+class Animal {
+  String name = '';
+
+  Animal(String name){
+    this.name = name;
+    print(name);
+  }
+}
---
+++ 13-public-private-scope/main.dart
@@ -0,0 +1,11 @@
+import 'animal.dart';
+main(List<String> arguments) {
+  Animal cat = new Animal('fluffy', 16, 'Short Hair');
+
+  cat.breed = 'mixed';
+  //cat._name = 'muffin';
+  //cat._display('hello');
+
+  cat.sayHello();
+  cat.saySomething('meow');
+}
