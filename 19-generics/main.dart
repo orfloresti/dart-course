c496bd9cab06eefc6bf582322e974d7bcb52bfd7
Orlando Flores Teomitzi <orlando.floresti@gmail.com>
2024-05-11 00:26:46 -0600

    generics

--- 19-generics/main.dart
+++ 19-generics/main.dart
@@ -1,3 +1,19 @@
 void main(List<String> args) {
-  
+  List<int> numbers = [];
+  numbers.addAll([1,2,3,4]);
+
+  print(numbers);
+
+  List<String> strings = [];
+  strings.addAll(['a', 'b', 'c']);
+
+  print(strings);
+  add(1, 2);
+  add(1.0, 2.0);
+  // add('Hello ', 'world'); // Not allowed for the type of params
 }
+
+void add<T extends num>(T a, T b)  {
+  print(a + b);
+}
+
