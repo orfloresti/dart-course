1b81b798c6e071d27951144b1521ac87aaf4a18b
Orlando Flores Teomitzi <orlando.floresti@gmail.com>
2024-09-30 18:04:40 -0600

    Exercise of zip

---
+++ 28-zip-files/example/zip_files_example.dart
@@ -0,0 +1,6 @@
+import 'package:zip_files/zip_files.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ 28-zip-files/test/zip_files_test.dart
@@ -0,0 +1,16 @@
+import 'package:zip_files/zip_files.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
