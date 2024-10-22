0b9bdc61d56fb9456fa51406d5101d4c0de30486
Orlando Flores Teomitzi <orlando.floresti@gmail.com>
2024-10-21 18:27:11 -0600

    Example of zip

--- 28-zip-files/bin/zip_files.dart
+++ 28-zip-files/bin/zip_files.dart
@@ -1,6 +1,53 @@
-import 'package:zip_files/zip_files.dart';
+import 'dart:io';
+import 'dart:convert';
+import 'package:archive/archive.dart';
+import 'package:path/path.dart' as p;
 
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+main(List<String> arguments) {
+
+  List<String> files = [];
+  Directory.current.listSync().forEach((FileSystemEntity fse) {
+    if(fse.statSync().type == FileSystemEntityType.file) files.add(fse.path);
+  });
+
+  String zipfile = '/Users/bryancairns/Downloads/compressed.zip';
+
+  zip(files, zipfile);
+
+  unzip(zipfile, '/Users/bryancairns/Downloads/decompressed');
+
+}
+
+void zip(List<String> files, String file) {
+  Archive archive = new Archive();
+
+  files.forEach((String path){
+    File file = new File(path);
+
+    ArchiveFile archivefile = new ArchiveFile(p.basename(path), file.lengthSync(), file.readAsBytesSync());
+    archive.addFile(archivefile);
+
+  });
+
+  ZipEncoder encoder = new ZipEncoder();
+  File f = new File(file);
+  f.writeAsBytesSync(encoder.encode(archive));
+
+  print('Compressed');
+
+}
+
+void unzip(String zip, String path) {
+  File file = new File(zip);
+
+  Archive archive = new ZipDecoder().decodeBytes(file.readAsBytesSync());
+
+  archive.forEach((ArchiveFile archivefile) {
+    File file = new File(path + '/' + archivefile.name);
+    file.createSync(recursive: true);
+    file.writeAsBytesSync(archivefile.content);
+
+  });
+
+  print('Decompressed');
 }
