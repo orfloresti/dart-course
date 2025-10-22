82523d0899c12f32aea937fc591441fe0130772c
Orlando Flores Teomitzi <orlando.floresti@gmail.com>
2025-10-21 18:11:55 -0600

    feat: Add initial implementation of UDP socket with basic structure and configuration files

---
+++ 38-udp-socket/bin/udp_socket.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+import 'dart:convert';
+
+main(List<String> arguments) {
+
+  var data = 'Hello World';
+
+  List<int> dataToSend = utf8.encode(data);
+  int port = 3000;
+
+  //Server
+  RawDatagramSocket.bind(InternetAddress.loopbackIPv4, port).then((RawDatagramSocket udpSocket){
+    udpSocket.listen((RawSocketEvent event) {
+      if(event == RawSocketEvent.read) {
+        Datagram? dg = udpSocket.receive();
+        if (dg != null) {
+          print(utf8.decode(dg.data));
+        }
+      }
+    });
+
+
+    //client
+    udpSocket.send(dataToSend, InternetAddress.loopbackIPv4, port);
+    print('Sent!');
+  });
+}
